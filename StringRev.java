c9ab8b70fb75789de09a62bda2a582e48b850784
Varun Devata <77072496+varun-devata@users.noreply.github.com>
2022-09-19 00:06:13 +0530

    Add files via upload

---
+++ StringRev.java
@@ -0,0 +1,8 @@
+//Reversing a string using StringBuffer class
+public class StringRev {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("My name is Varun");
+        StringBuffer s = sb.reverse();
+        System.out.println(s);
+    }
+}
