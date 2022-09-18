51a3a58ff9973aeb303d26b0ced4b5f7c8acd86c
Varun Devata <77072496+varun-devata@users.noreply.github.com>
2022-09-19 01:24:58 +0530

    Delete StringRev.java

--- StringRev.java
+++
@@ -1,8 +0,0 @@
-//Reversing a string using StringBuffer class
-public class StringRev {
-    public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("My name is Varun");
-        StringBuffer s = sb.reverse();
-        System.out.println(s);
-    }
-}
