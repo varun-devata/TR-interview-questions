94ba44f5890ca24be234957e99358f75a79d0c93
Varun Devata <77072496+varun-devata@users.noreply.github.com>
2022-09-26 21:09:20 +0530

    Add files via upload

---
+++ PassByReference.java
@@ -0,0 +1,23 @@
+
+public class PassByReference {
+	
+	public void fun(int[] arr) {
+		arr[1] = 90;
+		System.out.println("Marks of 2nd subject inside method " + arr[1]);
+	}
+
+	public static void main(String[] args) {
+		
+		int[] marks = {45, 60, 72};
+		
+		System.out.println("Marks of 2nd subject before changing " + marks[1]);
+		
+		PassByReference obj = new PassByReference();
+		
+		obj.fun(marks);
+		System.out.println("Marks of 2nd subject after changing " + marks[1]);
+		
+
+	}
+
+}
