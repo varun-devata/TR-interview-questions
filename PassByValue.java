57c8520228a12ec24e28e6b813f9f08c078d0e8d
Varun Devata <77072496+varun-devata@users.noreply.github.com>
2022-09-26 20:56:48 +0530

    Add files via upload

---
+++ PassByValue.java
@@ -0,0 +1,19 @@
+
+public class PassByValue {
+	
+	public void fun(int a) {
+		a++;
+		System.out.println("Value of a inside fun metod " + a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("Value of a before passing " + a);
+		PassByValue obj = new PassByValue();
+		obj.fun(a);
+		System.out.println("Value of a after passing " + a);
+	}
+	
+
+}
